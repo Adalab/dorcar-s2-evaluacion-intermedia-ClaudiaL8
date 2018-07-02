56775a9d9cc9ea66f41ad6ed4f65d1fe62927bc3
Claudia <cl.calerolopez@gmail.com>
2018-07-02 21:41:20 +0200

    pillar número usuario

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+'use strict'
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
-console.log('> ' + getRandomNumber(100));
 var alazar = getRandomNumber(100);
+console.log('> ' + alazar);
+
+
 var number = document.querySelector('.form__input');
 var boton = document.querySelector('.form__buttom');
+var userNumber;
 function click() {
     console.log(number.value);
 }
+
 boton.addEventListener('click', click );
   
 
