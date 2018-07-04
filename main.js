eed10e529c0fed690c02fd50314eba8274f029bf
Claudia <cl.calerolopez@gmail.com>
2018-07-04 19:39:40 +0200

    condicionales con error

--- main.js
+++ main.js
@@ -12,11 +12,20 @@ var boton = document.querySelector('.form__buttom');
 var userNumber;
 var contador = document.querySelector('.main__contador');
 var counter = 0;
+var parrafo = document.querySelector('.footer__parrafo');
 function click() {
-    console.log(number.value);
+    userNumber = parseInt(number.value);
+    console.log('El usuario ha elegido el ' + userNumber);
     counter++;
     contador.innerHTML = counter;
-}
+    if (alazar === userNumber){
+        parrafo.innerHTML = "¡HAS GANADO, CAMPEONA!";
+    } else if (alazar > userNumber){
+        parrafo.innerHTML = "¡Te has quedado corto!";
+    } else (alazar < userNumber){
+        parrafo.innerHTML = "¡Te has pasado!";
+    }
+    }
 
 boton.addEventListener('click', click );
   
